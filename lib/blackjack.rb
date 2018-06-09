5f5d0cf9de54bdc5a426ecf2e471766bbf24c37f
Shumei Huang <rose1264@gmail.com>
2018-06-09 01:57:50 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,8 +18,8 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, your card total is #{total}, thank you for playing!"
 end
 
 def initial_round
